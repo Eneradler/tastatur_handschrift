502798fcb2e5188fb9d7e93abf28f4f08a5e16b0
Bohdan Dakhno <bdakhno@gmail.com>
2018-01-20 01:46:31 +0100

    responsive design fuer kleinere geraete fertig gemacht

--- authenticate.php
+++ authenticate.php
@@ -1,39 +1,28 @@
 <!DOCTYPE HTML>
 <?php
-    $M1=$_POST['M1'];
-    //echo $M1;
-    //echo "  ";
-    $N1=$_POST['N1'];
-    //echo $N1;
-    //echo "  ";
-    $f1=fopen("presstime.txt","r");
-    $M=fgets($f1);
-    //echo $M;
-    //echo "  ";
+	// Get data from client
+    $M1 = $_POST['M1'];
+    $N1 = $_POST['N1'];
+	// Open files with biometric patterns und get them
+    $f1 = fopen("presstime.txt", "r");
+    $M = fgets($f1);
     fclose($f1);
-    $f2=fopen("betweentime.txt","r");
-    $N=fgets($f2);
-    //echo $N;
-    //echo "  ";
+    $f2 = fopen("betweentime.txt", "r");
+    $N = fgets($f2);
     fclose($f2);
-    /*if(($M=="") || ($N="")){
-    echo "Нет биометрического эталона!";
-    }
-    else{*/
-    $porog=10;
-    $porog2=30;
-    $Mlow=$M-$porog;
-    //echo "Mlow=";
-    //echo $Mlow;
-    //echo " ";
-    $Mhigh=$M+$porog;
-    $Nlow = $N-$porog2;
-    $Nhigh = $N+$porog;
-    if((($M1>=$Mlow) && ($M1<=$Mhigh)) && (($N1>=$Nlow) && ($N1<=$Nhigh))){
+    // Take two values for the interval
+    $porog = 10;
+    $porog2 = 30;
+	// Calculate interval of biometric pattern
+    $Mlow = $M - $porog;
+    $Mhigh = $M + $porog;
+    $Nlow = $N - $porog2;
+    $Nhigh = $N + $porog;
+	// Authentication passed if the data from client is in corresponding interval
+    if((($M1 >= $Mlow) && ($M1 <= $Mhigh)) && (($N1 >= $Nlow) && ($N1 <= $Nhigh))){
         echo "Congratulations! You have passed!";
     }
     else{
         echo "You have not passed.";
     }
-    //}
 ?>
--- training.php
+++ training.php
@@ -1,17 +1,21 @@
 
 <?php
     if (isset($_POST['M'])){
-        $prstime=$_POST['M'];
-        $btwntime=$_POST['N'];
-        $new_file1="presstime.txt";
-        $new_file2="betweentime.txt";
-        $f1=fopen($new_file1,"a+");
-        $f2=fopen($new_file2,"a+");
-        $d1=fgets($f1);
-        $d2=fgets($f2);
-        if(($d1!="")||($d2!="")){
+	    // Get data from client
+        $prstime = $_POST['M'];
+        $btwntime = $_POST['N'];
+		// Read files of biometric pattern
+        $new_file1 = "presstime.txt";
+        $new_file2 = "betweentime.txt";
+        $f1 = fopen($new_file1, "a+");
+        $f2 = fopen($new_file2, "a+");
+        $d1 = fgets($f1);
+        $d2 = fgets($f2);
+		// Error message, if pattern already exists
+        if(($d1 != "")||($d2 != "")){
             echo "You have already complete the training mode! Go to the identification";
         }
+		// Create pattern if not
         else{
             fwrite($f1,$prstime);
             fwrite($f1,"\r\n");
